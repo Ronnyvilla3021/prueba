64f8cd7a5516f5cb1587bb8aa93d9fc1140d7921
Ronnyvilla3021 <ronnievillavilla@gmail.com>
2025-10-23 21:43:54 -0500

    primero

---
+++ suma.py
@@ -0,0 +1,15 @@
+def sumar(a, b):
+    """Devuelve la suma de dos números."""
+    return a + b
+
+
+# ✅ Bloque principal (se ejecutará en GitHub Actions también)
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    resultado = sumar(a, b)
+    print(f"La suma de {a} + {b} es = {resultado}")
+
+    # Prueba simple automática
+    assert resultado == 15, "❌ La suma no es correcta"
+    print("✅ Prueba pasada correctamente")
